cb8a669096bf8698764231f073906891de04601c
OlguinJv <olguin.j.vito@gmail.com>
2018-10-26 05:19:53 -0300

    fast commit

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,25 @@ function toggleBurgerClass(part, class1, class2) {
         part.classList.add(class1);
     }
 }
+
+
+//---------------------------------------
+
+/*A PARTIR DE ACÁ SE CREA FUNCION PARA MOSTRAR EL BOTÓN DE MENÚ DE ACUERDO AL ANCHO DEL NAVEGADOR*/
+
+function showBurger(){
+    var anchoVentana = window.outerWidth;
+    var menu = document.getElementById()
+    if (anchoVentana < 711){
+        menu.classList.remove("menu-off");
+        menu.classList.add("menu-on");
+    }
+}
+
+// function hideMenu(){
+//     var ventana = window.outerWidth;
+//     if (ventana > 870){
+//         menu.classList.remove("menu-on");
+//         menu.classList.add("menu-off");
+//     }
+// }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,42 +1,43 @@
-
-function toggleClass() {
-    var button = document.getElementById("button");
-    var menu = document.getElementById("menu");
+// function toggleClass() {
+//     var button = document.getElementById("button");
+//     var menu = document.getElementById("menu");
     
-    if (menu.classList.contains("menu-off")) {
-        menu.style.opacity = "1";
-        menu.classList.remove("menu-off");
-        menu.classList.add("menu-on");
-    } else if (menu.classList.contains("menu-on")){
-        menu.classList.remove("menu-on");
-        menu.classList.add("menu-off");
-    }
+//     if (menu.classList.contains("menu-off")) {
+//         menu.style.opacity = "1";
+//         menu.classList.remove("menu-off");
+//         menu.classList.add("menu-on");
+//     } else if (menu.classList.contains("menu-on")){
+//         menu.classList.remove("menu-on");
+//         menu.classList.add("menu-off");
+//     }
     
-}
+// }
 
-var menuButton = document.getElementsByClassName("menu-button");
-console.log(menuButton);
+// var menuButton = document.getElementsByClassName("menu-button");
+// console.log(menuButton);
 
-function menuButtons() {
-    for (var i = 0; i < menuButton.length; i++) {
-            menuButton[i].addEventListener("click", function(e){
-            menu.classList.remove("menu-on");
-            menu.classList.add("menu-off");
-        })
-    }
-}
 
-function hideMenu(){
-    var ventana = window.outerWidth;
-    if (ventana > 870){
-        menu.classList.remove("menu-on");
-        menu.classList.add("menu-off");
-    }
-}
+// /* esta funcion hace que al clickear en un botón del menú, este se oculte */
+// function menuButtons() {
+//     for (var i = 0; i < menuButton.length; i++) {
+//             menuButton[i].addEventListener("click", function(e){
+//             menu.classList.remove("menu-on");
+//             menu.classList.add("menu-off");
+//         })
+//     }
+// }
 
-window.onload = function(){
-    menuButtons();
-}
+// function hideMenu(){
+//     var ventana = window.outerWidth;
+//     if (ventana > 870){
+//         menu.classList.remove("menu-on");
+//         menu.classList.add("menu-off");
+//     }
+// }
+
+// window.onload = function(){
+//     menuButtons();
+// }
 
 
 /*
