c4fb0929c10bb1ba7b98265351dd854e89abb2a0
OlguinJv <olguin.j.vito@gmail.com>
2018-10-26 22:23:56 -0300

    fast commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,41 @@
-var button = document.getElementById("burger");
+var burgerMenu = document.getElementById("burger-button");
+var burgerButton = document.getElementById("burger");
 var burgerTop = document.getElementById("burger-top");
 var burgerMiddle = document.getElementById("burger-middle");
 var burgerBottom = document.getElementById("burger-bottom");
-
+var menu = document.getElementById("menu");
+var burgerState = true;
 
 function activateBurguer(){
 
-    toggleBurgerClass(burgerTop, "top-animate", "top-deanimate");
-    toggleBurgerClass(burgerMiddle, "middle-animate", "middle-deanimate");
-    toggleBurgerClass(burgerBottom, "bottom-animate", "bottom-deanimate");
+    if (burgerState){
+        burgerState = false;
+    }
+    else {
+        burgerState = true;
+    }
 
-    button.classList.toggle("bounce");
+    toggleClass(burgerTop, "top-animate", "top-deanimate");
+    toggleClass(burgerMiddle, "middle-animate", "middle-deanimate");
+    toggleClass(burgerBottom, "bottom-animate", "bottom-deanimate");
+
+    burgerButton.classList.toggle("bounce");
     setTimeout(function(){backToDefault()}, 500);
 
 }
 
+function fullMenu(){
+    activateBurguer();
+    toggleClass(menu, "menu-background-off", "menu-background-on");
+}
+
 function backToDefault(){
-    button.classList.toggle("bounce");
+    burgerButton.classList.toggle("bounce");
 }
 
 ///////////////////////
 // ESTA FUNCIÓN SIRVE PARA TOGGLEAR CLASES A UN ELEMENTO DADO 
-function toggleBurgerClass(part, class1, class2) {
+function toggleClass(part, class1, class2) {
     if ( (part.classList.contains(class1) == false) &&  (part.classList.contains(class2) == false)) {
         part.classList.add(class1);
     } else if (part.classList.contains(class1)) {
@@ -33,24 +47,16 @@ function toggleBurgerClass(part, class1, class2) {
     }
 }
 
-
-//---------------------------------------
-
-/*A PARTIR DE ACÁ SE CREA FUNCION PARA MOSTRAR EL BOTÓN DE MENÚ DE ACUERDO AL ANCHO DEL NAVEGADOR*/
-
+//esta función sirve para devolver al menú hamburguesa a su estado original luego del resize//
 function showBurger(){
     var anchoVentana = window.outerWidth;
-    var menu = document.getElementById()
-    if (anchoVentana < 711){
-        menu.classList.remove("menu-off");
-        menu.classList.add("menu-on");
+    console.log(anchoVentana);
+    if (anchoVentana > 710 && burgerState == false){
+            activateBurguer();
+        if (!burgerState){
+        }
+    }
+    else {
+        
     }
 }
-
-// function hideMenu(){
-//     var ventana = window.outerWidth;
-//     if (ventana > 870){
-//         menu.classList.remove("menu-on");
-//         menu.classList.add("menu-off");
-//     }
-// }
